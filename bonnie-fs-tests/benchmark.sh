8e2beae3709b7352aca48e3cf7205db28c5f5a44
Colin Ian King <colin.king@canonical.com>
2009-07-08 09:11:57 +0100

    Quick and dirty bonnie test to be run across a variety of filesystems.

---
+++ bonnie-fs-tests/benchmark.sh
@@ -0,0 +1,38 @@
+#!/bin/bash -x
+
+#
+# Copyright (C) 2009 Canonical
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+#
+
+#
+# Author Colin Ian King,  colin.king@canonical.com
+#
+
+
+DEV=/dev/mmcblk0p1
+
+for I in ext2 ext3 ext4 xfs jfs reiserfs vfat
+do
+	echo Filesystem: $I
+	echo y | mkfs.$I $DEV
+	sync; sleep 1; sync; sleep 1; sync
+	fsck $DEV
+	mount -o $DEV /mnt
+	mkdir /mnt/test
+	bonnie++ -d /mnt/test -r 1 -s 7 -u 0:0 
+	umount /mnt
+done
